0c1d32cb9e83d555e16f52505f6268dc99d357c6
Stanislav Staev <s.staev1@gmail.com>
2022-05-10 15:38:41 +0200

    udpate text

--- components/Product.js
+++ components/Product.js
@@ -126,8 +126,8 @@ export default function Product({ pp, id }) {
             <p
               style={{ fontStyle: "italic", fontSize: "10px", width: "205px" }}
             >
-              Pre-order Ordering is open until May 31st. Distribution begins
-              effectively on June 21st. For further questions please contact us.
+              Ordering is open until May 31st. Distribution begins effectively
+              on June 21st. For further questions please contact us.
             </p>
           </div>
           <div className="img-container">
